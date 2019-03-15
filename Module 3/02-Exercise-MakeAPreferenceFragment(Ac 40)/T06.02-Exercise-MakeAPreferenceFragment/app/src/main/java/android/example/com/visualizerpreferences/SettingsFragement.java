8078200cef044718ae10b5a1f1c9c844ef124a90
tezasrivishnu <tezasri.vishnu@msitprogram.net>
2019-03-15 11:47:55 +0530

    M3 ac 40

---
+++ Module 3/02-Exercise-MakeAPreferenceFragment(Ac 40)/T06.02-Exercise-MakeAPreferenceFragment/app/src/main/java/android/example/com/visualizerpreferences/SettingsFragement.java
@@ -0,0 +1,10 @@
+package android.example.com.visualizerpreferences;
+import android.os.Bundle;
+import android.support.v7.preference.PreferenceFragmentCompat;
+
+public class SettingsFragement extends PreferenceFragmentCompat  {
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        addPreferencesFromResource(R.xml.pref_visualizer);
+    }
+}
